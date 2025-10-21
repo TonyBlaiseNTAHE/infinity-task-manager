199e0bcd9aeb70c48ab66a09cadd8aec7504d445
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2025-10-21 10:59:59 +0200

    creating task updates funtion

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -6,11 +6,69 @@ import Task from '../models/Task.js';
   */
  export const getTasks = async (req, res) => {
     try {
-        
-    } catch (error) {
-        res.status(500).json({message: "Server Error", error: error.message});
+        const {status} = req.query;
+        let filter = {};
+
+        if (status){
+            filter.status = status;
+        }
+        let tasks;
+
+        if (req.employee.role === "admin"){
+            tasks = await Task.find(filter).populate(
+                "assignedTo",
+                "name email profileImageUrl"
+            );
+
+        } else {
+            tasks = await Task.find({ ...filter, assignedTo: req.employee._id }).populate(
+                "assignedTo",
+                "name email profileImageUrl"
+            );
+        }
+
+        /**
+         * Add completed todoChecklist count to each task
+         */
+        tasks = await Promise.all(
+            tasks.map(async (task) => {
+                const completedCount = task.todoChecklist.filter(
+                    (item) => item.completed).length;
+                    return { ...task._doc, completedTodoCount: completedCount};
+                })
+            );
+      
+    /**
+     * Status summary counts
+     */
+    const allTasks = await Task.countDocuments(req.employee.role === "admin" ? {} : {assignedTo: req.employee._id});
+    const pendingTasks = await Task.countDocuments({
+        ...filter,
+        status: "Pending",
+        ...(req.employee.role !== "admin" && { assignedTo: req.employee._id}),
+    });
+    const inProgressTasks = await Task.countDocuments({
+        ...filter,
+        status: "In Progress",
+        ...(req.employee.role !== "admin" && { assignedTo: req.employee._id}),
+    });
+    const completedTasks = await Task.countDocuments({
+        ...filter,
+        status: "Completed",
+        ...(req.employee.role !== "admin" && { assignedTo: req.employee._id}),
+    });
+    res.json ({
+        tasks,
+        statusSummary: {
+            all: allTasks,
+            pendingTasks,
+            inProgressTasks,
+            completedTasks,
+        },
+    });
+    } catch (error)
+        {res.status(500).json({message: "Server Error", error: error.message});}
     }
- }
 
  /**
   * descr: Get task By ID
@@ -19,7 +77,11 @@ import Task from '../models/Task.js';
   */
  export const getTaskById = async (req, res) => {
     try {
-        
+        const task = await Task.findById(req.params.id).populate("assignedTo", "name email profileImageUrl");
+
+        if (!task) return res.status(404).json({ message: "Task not found"});
+
+        res.json(task);
     } catch (error) {
         res.status(500).json({message: "Server Error", error: error.message});
     }
@@ -60,6 +122,24 @@ import Task from '../models/Task.js';
   */
  export const updateTask = async (req, res) => {
     try {
+        const task = await Task.findById(req.params.id);
+        if(!task) return res.status(404).json({ message: "Task not found"});
+
+        task.title = req.body.title || task.title;
+        task.description = req.body.description || task.description;
+        task.priority = req.body.priority || task.priority;
+        task.dueDate = req.body.dueDate || task.dueDate;
+        task.todoChecklist = req.body.todoChecklist || task.todoChecklist;
+        task.attachments = req.body.attachments || task.attachments;
+
+        if (req.body.assignedTo){
+            if (!Array.isArray(req.body.assignedTo)){
+                return res.status(400).json({ message: "assignedTo must be an array of employee IDs"});
+            }
+            task.assignedTo = req.body.assignedTo;
+        }
+        const updatedTask = await task.save();
+        res.json( { message: "Task updated successfully", updatedTask})
         
     } catch (error) {
         res.status(500).json({message: "Server Error", error: error.message});
